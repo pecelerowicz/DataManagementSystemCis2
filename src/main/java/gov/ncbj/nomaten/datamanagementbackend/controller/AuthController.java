5957b5a89213bea445557133c73b80b6d4005a33
Michał Pecelerowicz <michal.pecelerowicz@ncbj.gov.pl>
2021-11-29 09:31:21 +0100

    fixes

--- src/main/java/gov/ncbj/nomaten/datamanagementbackend/controller/AuthController.java
+++ src/main/java/gov/ncbj/nomaten/datamanagementbackend/controller/AuthController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api")
 @AllArgsConstructor
 @CrossOrigin
 public class AuthController {
@@ -22,17 +22,17 @@ public class AuthController {
     private final AuthService authService;
     private final RefreshTokenService refreshTokenService;
 
-    @PostMapping("/login")
+    @PostMapping("/auth/login")
     public AuthenticationResponse login(@RequestBody LoginRequest loginRequest) {
         return authService.login(loginRequest);
     }
 
-    @PostMapping("refresh/token")
+    @PostMapping("/auth/refresh/token")
     public AuthenticationResponse refreshTokens(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
         return authService.refreshToken(refreshTokenRequest);
     }
 
-    @PostMapping("/logout")
+    @PostMapping("/auth/logout")
     public ResponseEntity<String> logout(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
         refreshTokenService.deleteRefreshToken(refreshTokenRequest.getRefreshToken());
         return ResponseEntity.status(OK).body("Refresh Token deleted successfully.");
@@ -45,7 +45,7 @@ public class AuthController {
     }
 
     // TODO zrobić porządne dto
-    @GetMapping("/users")
+    @GetMapping("/auth/users")
     public ResponseEntity<List<String>> getUsers() {
         return ResponseEntity.status(OK).body(authService.getUsers());
     }
