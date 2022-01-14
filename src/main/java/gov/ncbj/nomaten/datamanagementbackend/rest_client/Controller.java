b2f8d81f4b6fe5143dc0595cdf48443ab375b813
Michał Pecelerowicz <michal.pecelerowicz@ncbj.gov.pl>
2022-01-14 12:42:20 +0100

    Add multiple users

--- src/main/java/gov/ncbj/nomaten/datamanagementbackend/rest_client/Controller.java
+++ src/main/java/gov/ncbj/nomaten/datamanagementbackend/rest_client/Controller.java
@@ -2,7 +2,6 @@ package gov.ncbj.nomaten.datamanagementbackend.rest_client;
 
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
